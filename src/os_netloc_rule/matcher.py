5e8d6d69994f519f94328f2ed9373274a4eb1d4b
Ozzy <cfhamlet@gmail.com>
2019-11-11 15:42:48 +0800

    support apis

--- src/os_netloc_rule/matcher.py
+++ src/os_netloc_rule/matcher.py
@@ -5,8 +5,8 @@ class Matcher(object):
     def __init__(self):
         self._root = Node()
 
-    def match(self, domain_with_port):
-        return match(self._root, domain_with_port)
+    def match(self, domain_with_port, schema=None):
+        return match(self._root, domain_with_port, schema)
 
-    def load(self, domain_with_port, rule, cmp=None):
+    def load(self, domain_with_port, rule, schema=None, cmp=None):
         return load(self._root, domain_with_port, rule, cmp=cmp)

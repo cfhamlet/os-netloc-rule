891ad575bf82f74e84112ea4b9d1efcc6a2dce38
Ozzy <cfhamlet@gmail.com>
2019-11-11 15:43:34 +0800

    test: matcher

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -8,15 +8,65 @@ def matcher():
 
 
 def load_and_match(matcher, rules, cases):
-    for domain_with_port, rule, cmp in rules:
-        matcher.load(domain_with_port, rule, cmp=cmp)
+    for rule in rules:
+        matcher.load(*rule)
 
     for domain_with_port, expected in cases:
         assert matcher.match(domain_with_port) == expected
 
 
 def test_matcher_001(matcher):
-    rules = []
-    cases = []
+    rules = [
+        ("www.example.com", 1),
+        ("example.com", 2),
+        ("example.com:88", 3),
+        ("test.com", 4),
+    ]
+    cases = [
+        ("www.example.com", 1),
+        ("abc.example.com", 2),
+        ("123.example.com:88", 3),
+        ("test.com:88", 4),
+    ]
+
+    load_and_match(matcher, rules, cases)
+
+
+def test_matcher_002(matcher):
+    rules = [
+        (".example.com", 1),
+        ("example.com", 2),
+    ]
+    cases = [
+        ("www.example.com", 1),
+        ("example.com", 2),
+    ]
+
+    load_and_match(matcher, rules, cases)
+
+
+def test_matcher_003(matcher):
+    rules = [
+        ("www.example.com", 1),
+        ("www.example.com", 2),
+    ]
+    cases = [
+        ("www.example.com", 2),
+    ]
+
+    load_and_match(matcher, rules, cases)
+
+
+def test_matcher_003(matcher):
+    def cmp(x, y):
+        return x if x > y else y
+
+    rules = [
+        ("www.example.com", 2, None, cmp),
+        ("www.example.com", 1, None, cmp),
+    ]
+    cases = [
+        ("www.example.com", 2),
+    ]
 
     load_and_match(matcher, rules, cases)

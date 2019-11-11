bf0e923272511d456566720ccd108635593a8b0b
Ozzy <cfhamlet@gmail.com>
2019-11-11 14:51:29 +0800

    update: load and match api

--- src/os_netloc_rule/matcher.py
+++ src/os_netloc_rule/matcher.py
@@ -1,12 +1,16 @@
+from .const import Symbols
 from .netloc_reversed_tree import Node, load, match
+from .utils import split_domain_port
 
 
 class Matcher(object):
     def __init__(self):
         self._root = Node()
 
-    def load(self, pieces, port, rule):
-        return load(self._root, pieces, port, rule)
+    def match(self, domain_with_port):
+        domain, port = split_domain_port(domain_with_port)
+        return match(self._root, domain.split(Symbols.DOT), port)
 
-    def match(self, domain, port):
-        return match(self._root, domain, port)
+    def load(self, domain_with_port, rule, cmp=None):
+        domain, port = split_domain_port(domain_with_port)
+        return load(self._root, domain.split(Symbols.DOT), port, rule, cmp=cmp)

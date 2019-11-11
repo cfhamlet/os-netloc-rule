9cc434df68034256283423c1b873655f7f135577
Ozzy <cfhamlet@gmail.com>
2019-11-11 21:18:32 +0800

    test: matcher dump

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -112,3 +112,19 @@ def test_matcher_006(matcher):
     ]
 
     load_and_match(matcher, rules, cases)
+
+
+def test_matcher_dump(matcher):
+    rules = [
+        ("www.example.com:80", 1),
+        ("www.example.com:8080", 2),
+        (".example.com:8080", 3),
+        (".example.com", 4),
+    ]
+
+    for domain_with_port, rule in rules:
+        matcher.load(domain_with_port, rule)
+
+    o = list(matcher.dump())
+
+    assert set(o) == set(rules)
